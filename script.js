71c53be275c43e93ed3227225c22fd59188ffc39
Arbaj Ansari <83424578+ArbajAr7@users.noreply.github.com>
2024-05-22 18:32:27 +0545

    created a submission

--- script.js
+++ script.js
@@ -12,50 +12,125 @@ var audioPlayer5 = document.getElementById('audioPlayer5');
 var playButton6 = document.getElementById('btn6');
 var audioPlayer6 = document.getElementById('audioPlayer6');
 function btn1() {
+			audioPlayer2.pause();
+            audioPlayer2.currentTime = 0;
+			audioPlayer3.pause();
+            audioPlayer3.currentTime = 0;
+			audioPlayer4.pause();
+            audioPlayer4.currentTime = 0;
+			audioPlayer5.pause();
+            audioPlayer5.currentTime = 0;
+			audioPlayer6.pause();
+            audioPlayer6.currentTime = 0;
         // Add click event listener to the button
     playButton1.addEventListener('click', function() {
         audioPlayer1.play().catch(function(error) {
             console.error('Error playing audio:', error);
         });
     });
+}
 	function btn2() {
+			audioPlayer1.pause();
+            audioPlayer1.currentTime = 0;
+			audioPlayer3.pause();
+            audioPlayer3.currentTime = 0;
+			audioPlayer4.pause();
+            audioPlayer4.currentTime = 0;
+			audioPlayer5.pause();
+            audioPlayer5.currentTime = 0;
+			audioPlayer6.pause();
+            audioPlayer6.currentTime = 0;
         // Add click event listener to the button
     playButton2.addEventListener('click', function() {
         audioPlayer2.play().catch(function(error) {
             console.error('Error playing audio:', error);
         });
     });
+	}
 	function btn3() {
+			audioPlayer1.pause();
+            audioPlayer1.currentTime = 0;
+			audioPlayer2.pause();
+            audioPlayer2.currentTime = 0;
+			audioPlayer4.pause();
+            audioPlayer4.currentTime = 0;
+			audioPlayer5.pause();
+            audioPlayer5.currentTime = 0;
+			audioPlayer6.pause();
+            audioPlayer6.currentTime = 0;
         // Add click event listener to the button
     playButton3.addEventListener('click', function() {
         audioPlayer3.play().catch(function(error) {
             console.error('Error playing audio:', error);
         });
     });
+	}
 	function btn4() {
+			audioPlayer1.pause();
+            audioPlayer1.currentTime = 0;
+			audioPlayer2.pause();
+            audioPlayer2.currentTime = 0;
+			audioPlayer3.pause();
+            audioPlayer3.currentTime = 0;
+			audioPlayer5.pause();
+            audioPlayer5.currentTime = 0;
+			audioPlayer6.pause();
+            audioPlayer6.currentTime = 0;
         // Add click event listener to the button
     playButton4.addEventListener('click', function() {
         audioPlayer4.play().catch(function(error) {
             console.error('Error playing audio:', error);
         });
     });
+	}
 	function btn5() {
+			audioPlayer1.pause();
+            audioPlayer1.currentTime = 0;
+			audioPlayer2.pause();
+            audioPlayer2.currentTime = 0;
+			audioPlayer3.pause();
+            audioPlayer3.currentTime = 0;
+			audioPlayer4.pause();
+            audioPlayer4.currentTime = 0;
+			audioPlayer6.pause();
+            audioPlayer6.currentTime = 0;
         // Add click event listener to the button
     playButton5.addEventListener('click', function() {
         audioPlayer5.play().catch(function(error) {
             console.error('Error playing audio:', error);
         });
     });
+	}
 	function btn6() {
+		audioPlayer1.pause();
+            audioPlayer1.currentTime = 0;
+			audioPlayer2.pause();
+            audioPlayer2.currentTime = 0;
+			audioPlayer3.pause();
+            audioPlayer3.currentTime = 0;
+			audioPlayer4.pause();
+            audioPlayer4.currentTime = 0;
+			audioPlayer5.pause();
+            audioPlayer5.currentTime = 0;
         // Add click event listener to the button
     playButton6.addEventListener('click', function() {
         audioPlayer6.play().catch(function(error) {
             console.error('Error playing audio:', error);
         });
     });
-	function btn7() {
-			audioPlayer.pause();
-            audioPlayer.currentTime = 0;
 	}
-}
+	function btn7() {
+			audioPlayer1.pause();
+            audioPlayer1.currentTime = 0;
+			audioPlayer2.pause();
+            audioPlayer2.currentTime = 0;
+			audioPlayer3.pause();
+            audioPlayer3.currentTime = 0;
+			audioPlayer4.pause();
+            audioPlayer4.currentTime = 0;
+			audioPlayer5.pause();
+            audioPlayer5.currentTime = 0;
+			audioPlayer6.pause();
+            audioPlayer6.currentTime = 0;
+	}
 
